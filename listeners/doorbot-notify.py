a1bed939af96de1d153cfbb1682c9dd2946f44ce
Jonty <jonty@jonty.co.uk>
2011-02-20 13:16:59 +0000

    Switch doorbot-notify to implement DoorbotListener

--- listeners/doorbot-notify.py
+++ listeners/doorbot-notify.py
@@ -1,35 +1,29 @@
 #!/usr/bin/env python
-import select, socket, pynotify
+import DoorbotListener, pynotify
 
-if __name__ == "__main__":
+class NotifyListener(DoorbotListener.DoorbotListener):
 
-    if not pynotify.init("icon-summary-body"):
-        sys.exit(1)
+    def doorbell(self):
+        self.sendMessage(
+            "BING BONG!",
+            "Someone pressed the hackspace doorbell."
+        )
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('<broadcast>', 50000))
-    s.setblocking(0)
+    def doorOpened(self, serial, name):
+        self.sendMessage(
+            "Door opened",
+            "%s opened the hackspace door." % name
+        )
 
-    while True:
+    def unknownCard(self, serial):
+        self.sendMessage(
+            "ALERT",
+            "An unknown RFID card was used on the hackspace door."
+        )
 
-        result = select.select([s],[],[])
-        payload = result[0][0].recv(1024)
-        (event, card, name) = payload.split("\n")
+    def sendMessage(self, title, message):
+        pynotify.Notification(title, message).show()
 
-        title = message = ''
 
-        if (event == 'RFID' and name):
-            title   = "Door opened"
-            message = "%s opened the hackspace door." % name
-
-        elif (event == 'RFID' and not name):
-            title   = "ALERT"
-            message = "An unknown RFID card was used on the hackspace door."
-
-        elif (event == 'BELL'):
-            title   = "BING BONG!"
-            message = "Someone pressed the hackspace doorbell."
-
-        if title and message:
-            pynotify.Notification(title, message).show()
+listener = NotifyListener()
+listener.listen()
