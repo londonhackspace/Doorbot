ca20b8ab9ab37684a4e002a361a079e4cd147c9d
eb4890 <wil.pearson@gmail.com>
2011-02-24 05:24:51 +0000

    Doing the rest of the modifications. And getting used to git

--- listeners/glados/doorbot-glados.py
+++ listeners/glados/doorbot-glados.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys, os, time, re, socket, soundplayer, urllib2, random
+import sys, os, time, re, socket, urllib2, random, subprocess
 from datetime import datetime
 
 sys.path.append('../')
@@ -9,6 +9,21 @@ wavefile = "wavetable.dat"
 
 waves = {}
 randoms = []
+lastmodified = []
+
+
+def playSoundsBackground( sounds):
+  
+   files  = [os.getcwd() + "/" + s  for s in sounds ]
+   filesstring = " ".join(files)
+   string =  "/usr/bin/mplayer "+ filesstring+ " < /dev/null &"
+   #print string
+   subprocess.Popen( string, shell=True) 
+
+def playSounds(sounds):
+  for sound in sounds:
+    print "playing " + sound
+    subprocess.call(["mplayer", sound])
 
 
 def loadGreetings():
@@ -22,35 +37,65 @@ def loadGreetings():
     if match:
       id, name = match.groups()
       waves[id] = name
+      #print id  + " " + name
     else:
       print 'Invalid entry at line %d' % n
 
+def checkNewGreetings():
+   stat = os.stat (wavefile)
+   print lastmodified
+   if (not (stat.st_mtime in lastmodified)):
+     print "loading greetings"   
+     loadGreetings()
+     del lastmodified[0]
+     lastmodified.append(stat.st_mtime)
 def loadRandoms ():
   for f in os.listdir("./wavefiles/random"):
     randoms.append(f)
 
 def playGreeting(id):
   random.seed()
-  if id == '15FAA09E':
-    soundplayer.main(["wavefiles/fixed/hackspacewelcome.wav","wavefiles/members/jonty.wav"])
-  else:
-    if id in waves.keys():
-      if waves[id] == "broken.wav":
-        soundplayer.main([waves[id]])
-      else:
-        soundplayer.main(["wavefiles/fixed/hackspacewelcome.wav","wavefiles/fixed/hackspacetestsubject.wav", "wavefiles/members/" + waves[id], "wavefiles/fixed/hackspacedoor.wav" ])
+  print lastmodified
+  welcome = re.compile("^welcome-")
+  member = re.compile("^member-")
+  mfolder = "wavefiles/members/"
+  ffolder = "wavefiles/fixed/"
+  rfolder = "wavefiles/random/"
+  welcomesound = ffolder + "hackspacewelcome.wav"
+  
+  if id in waves:
+    
+    sounds = waves[id].split(' ')
+    mainsound  = sounds[len(sounds)-1]
+    memberfiles = [ mfolder + s  for s in sounds ]
+ # More than one sound, play the first lot in the background   
+    if len(sounds) > 1:
+      length = len(sounds)
+      playSoundsBackground (memberfiles[:length-1])
+      membersound = memberfiles [length-1]
     else:
-        soundplayer.main(["wavefiles/fixed/hackspacelonely.wav"])    
+      membersound =  mfolder + waves [id]
 
-    if random.randint(0,5) == 4:
-      r = random.randint(0,len(randoms) -1)
-      soundplayer.main(["wavefiles/random/" + randoms[r] ]) 
+    if member.match (mainsound):
+      playSounds([welcomesound ,ffolder+ "hackspacetestsubject.wav", membersound , ffolder +"hackspacedoor.wav" ])
+
+    elif welcome.match(mainsound):
+      playSounds([ffolder + "hackspacewelcome.wav", membersound])
+    else:
+      playSounds([membersound])
+  else:
+      playSounds([ffolder + "hackspacelonely.wav"])    
+
+  if random.randint(0,5) == 4:
+    r = random.randint(0,len(randoms) -1)
+    playSounds(["wavefiles/random/" + randoms[r] ]) 
+  checkNewGreetings()
   
 def playDenied():
-  soundplayer.main(["wavefiles/fixed/hackspacedenied.wav"])
+  playSounds(["wavefiles/fixed/hackspacedenied.wav"])
 
 def playDoorbell():
-  soundplayer.main(["wavefiles/fixed/hackspacebingbong.wav"])
+  playSounds(["wavefiles/fixed/hackspacebingbong.wav"])
 
 
 class GladosListener(DoorbotListener.DoorbotListener):
@@ -68,8 +113,9 @@ class GladosListener(DoorbotListener.DoorbotListener):
 
         try:
             playGreeting(serial)
-        except Exception:
-            pass
+        except Exception, e:
+           print e
+           pass
 
     def unknownCard(self, serial):
         print "Unknown card"
@@ -83,6 +129,7 @@ class GladosListener(DoorbotListener.DoorbotListener):
 if __name__ == "__main__":
     loadGreetings()
     loadRandoms()
-
+    lastmodified. append( os.stat(wavefile).st_mtime)
+    print lastmodified
     listener = GladosListener()
     listener.listen()
