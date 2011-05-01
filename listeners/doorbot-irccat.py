b7592404c6f3deee2c65bfe7b9cc0ddc9c9ee3c6
Jonty <jonty@jonty.co.uk>
2011-05-01 20:14:11 +0100

    Add 'last seen' to doorbot IRC announcements

--- listeners/doorbot-irccat.py
+++ listeners/doorbot-irccat.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
-import DoorbotListener, socket, random
+import DoorbotListener, socket, random, os, pickle, datetime
+
+def dayordinal(day):
+  if 4 <= day <= 20 or 24 <= day <= 30:
+    suffix = 'th'
+  else:
+    suffix = ['st', 'nd', 'rd'][day % 10 - 1]
+  return '%d%s' % (day, suffix)
+
+def untilmsg(until):
+    hours, seconds = divmod(until.seconds, 3600)
+    days = until.days
+    d_s = '' if days == 1 else 's'
+    h_s = '' if hours == 1 else 's'
+    
+    if hours ==0 and days == 0:
+        return '%s minutes' % int(seconds/60)
+    elif days == 0:
+        return '%s hour%s' % (hours, h_s)
+    elif hours == 0:
+        return '%s day%s' % (days, d_s)
+    else:
+        return '%s day%s, %s hour%s' % (days, d_s, hours, h_s)
+
+PICKLEFILE = '/usr/share/irccat/.lastseen.pickle'
+
 
 class IrccatListener(DoorbotListener.DoorbotListener):
 
@@ -18,7 +43,24 @@ class IrccatListener(DoorbotListener.DoorbotListener):
         self.sendMessage("BING BONG DOOR BELL! http://hackspace.org.uk:8003")
 
     def doorOpened(self, serial, name):
-        self.sendMessage("%s opened the hackspace door." % name)
+
+        lastseen = {}
+        if os.path.exists(PICKLEFILE):
+            lastseen = pickle.load(open(PICKLEFILE))
+
+        try:
+            d = lastseen[name.lower()]
+
+            self.sendMessage(
+                "%s opened the hackspace door. (Last seen %s ago)" % (
+                    name.title(),
+                    untilmsg(datetime.datetime.now() - d)
+                )
+            )
+
+        except KeyError:
+            self.sendMessage("%s opened the hackspace door." % name)
+
 
     def unknownCard(self, serial):
         self.sendMessage("Unknown card presented at the hackspace door.")
--- listeners/doorbot-lastseen.py
+++ listeners/doorbot-lastseen.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
-import DoorbotListener, os, pickle, datetime
+import DoorbotListener, os, pickle, datetime, time
 
 PICKLEFILE = '/usr/share/irccat/.lastseen.pickle'
 
 class LastSeenListener(DoorbotListener.DoorbotListener):
 
     def doorOpened(self, serial, name):
+
+        # To stop a race condition between us and the irc announce bot
+        time.sleep(2)
 
         print "%s opened the door, logging" % name
 
