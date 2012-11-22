77441231293fa6c095c4d41edd16ce1bca0771aa
Mark Steward <marksteward@gmail.com>
2012-11-22 17:59:03 +0000

    Grab a series of images for the doorbell

--- listeners/doorbot-camhandler.py
+++ listeners/doorbot-camhandler.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
-import DoorbotListener, os
+import DoorbotListener, os, time
 
 class CamhandlerListener(DoorbotListener.DoorbotListener):
 
     def doorbell(self):
-        os.system("grab-mjpeg.py localhost:8003 /var/www/doorbell.jpg")
+        print 'Doorbell rung: grabbing images'
+        os.system("/usr/local/bin/grab-mjpeg.py localhost:8003 /var/www/doorbell.jpg")
+        for n in range(5):
+            print '%s/5' % (n+1)
+            self.grabimage(n)
+
+    def grabimage(self, n):
+        os.system("/usr/local/bin/grab-mjpeg.py localhost:8003 /var/www/doorbell/%s.jpg" % n)
+        time.sleep(1)
 
     def unknownCard(self, serial):
-        os.system("grab-mjpeg.py localhost:8003 /var/www/doorbell.jpg")
+        print 'Unknown card: grabbing image'
+        os.system("/usr/local/bin/grab-mjpeg.py localhost:8003 /var/www/doorbell.jpg")
 
 
 listener = CamhandlerListener()
