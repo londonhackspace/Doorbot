ee01a90416be8b8175a016dd1931790984aa7a41
Mark Steward <marksteward@gmail.com>
2013-06-06 08:50:38 +0100

    Front door boarded and lastseen (also temporary)

---
+++ listeners/doorbot-front-boarded.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import DoorbotListener, urllib2
+
+class BoardedListener(DoorbotListener.DoorbotListener):
+
+    def doorbell(self):
+        self.sendMessage("BING BONG DOOR BELL")
+
+    def doorOpened(self, serial, name):
+        self.sendMessage("%s opened the hackspace door" % name)
+
+    def unknownCard(self, serial):
+        self.sendMessage("Unknown card presented at the hackspace door")
+
+    def sendMessage(self, message):
+        text = "http://172.31.24.5:8020/%s?restoreAfter=30" % urllib2.quote(message)
+        print text
+
+        try:
+            urllib2.urlopen(text)
+        except Exception, e:
+            pass
+
+
+listener = BoardedListener()
+listener.listen(50002)
---
+++ listeners/doorbot-front-lastseen.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import DoorbotListener, os, pickle, datetime, time
+
+PICKLEFILE = '/usr/share/irccat/.lastseen.pickle'
+
+class LastSeenListener(DoorbotListener.DoorbotListener):
+
+    def doorOpened(self, serial, name):
+
+        # To stop a race condition between us and the irc announce bot
+        time.sleep(2)
+
+        print "%s opened the front door, logging" % name
+
+        lastseen = {}
+
+        if os.path.exists(PICKLEFILE):
+            lastseen = pickle.load(open(PICKLEFILE))
+
+        lastseen[name.lower()] = datetime.datetime.now()
+
+        pickle.dump(lastseen, open(PICKLEFILE, 'wb'))
+
+
+if __name__ == '__main__':
+	listener = LastSeenListener()
+	listener.listen(50002)
