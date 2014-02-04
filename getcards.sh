e39ce4025ca10ffbaee626155a6f5da8cae6afb3
Jasper Wallace <jasper@pointless.net>
2014-02-04 16:27:29 +0000

    when fetching the carddb, only print stuff if there is an error

--- getcards.sh
+++ getcards.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /run/shm
-curl -kf -o carddb.json.download --compressed https://london.hackspace.org.uk/carddb.php && mv carddb.json.download carddb.json
+curl -s -S -kf -o carddb.json.download --compressed https://london.hackspace.org.uk/carddb.php && mv carddb.json.download carddb.json
 #curl -kf -o carddb.json.download --compressed https://85.119.83.146/carddb.php && mv carddb.json.download carddb.json
