040d0146de2c5b99bcc0c0a0eb85975a2f4f5396
Jasper Wallace <jasper@pointless.net>
2014-02-04 16:35:13 +0000

    Update this to match the announcer.py code, should of been commited a while ago

--- listeners/send-debug-broadcast.py
+++ listeners/send-debug-broadcast.py
@@ -37,7 +37,7 @@ else:
         sys.exit(1)
 
 s = socket(AF_INET, SOCK_DGRAM)
-s.bind(('', 0))
+s.bind(('<broadcast>', 0))
 s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-s.sendto(data, ('<broadcast>', 50000))
+s.sendto(data, ('<broadcast>', 50002))
