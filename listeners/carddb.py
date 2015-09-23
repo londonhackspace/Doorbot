364038c65fd7ebd1c481c187d1ff07a70c6990ee
Jan Szumiec <jan.szumiec@gmail.com>
2015-09-23 23:47:59 +0200

    When carddb.json is updated, reload it.

--- listeners/carddb.py
+++ listeners/carddb.py
@@ -1,12 +1,21 @@
-import json
+import json, time, os.path
 
 class CardDB:
     def __init__(self, filename="carddb.json"):
-        f = open(filename, "r")
-        self.db = json.load(f)
+        self.filename = filename
+        self.lastModified = time.ctime(os.path.getmtime(filename))
+        self.db = json.load(open(filename, "r"))
 
     def nickForCard(self, card_serial):
+        self.maybeRefresh()
         for entry in self.db:
             if card_serial in entry['cards']:
                 return entry['nick']
         return None
+
+    def maybeRefresh(self):
+        timestamp = time.ctime(os.path.getmtime(self.filename))
+        if timestamp > self.lastModified:
+            print 'refreshing'
+            self.lastModified = timestamp
+            self.db = json.load(open(self.filename, "r"))
