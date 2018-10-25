82bb3613a082c0e6c1cae393194f9c726154008c
Michael Cullen <michael@cullen-online.com>
2018-10-25 01:49:16 +0100

    Added first batch of MQTT listeners

---
+++ mqttlisteners/lastseen_listener.py
@@ -0,0 +1,28 @@
+import os, pickle, datetime, time
+from MQTTDoorbotListener import MQTTDoorbotListener
+
+class LastSeenListener(MQTTDoorbotListener):
+
+    def on_card(self, card_id, name, door):
+
+        # To stop a race condition between us and the irc announce bot
+        time.sleep(2)
+
+        print("%s opened the door, logging" % name)
+
+        lastseen = {}
+
+        picklefile = self.config.get('lastseen', 'picklefile')
+
+        if os.path.exists(picklefile):
+            lastseen = pickle.load(open(picklefile))
+
+        lastseen[name.lower()] = datetime.datetime.now()
+
+        pickle.dump(lastseen, open(picklefile, 'wb'))
+
+
+if __name__ == '__main__':
+    listener = LastSeenListener()
+    #listener.run()
+    listener.on_card("1234", "mich181189", listener.config['1f-entrance'])
---
+++ mqttlisteners/print_listener.py
@@ -0,0 +1,28 @@
+from MQTTDoorbotListener import MQTTDoorbotListener
+
+class TestListener(MQTTDoorbotListener):
+
+    def on_card(self, card_id, name, door):
+        if door.getboolean('announce', True):
+            print("%s presented card %s at door %s" % (name, card_id, door['name']))
+        else:
+            print("Will not announce stuff at %s" % (door['name'],))
+
+    def on_unknown_card(self, card_id, door):
+        print("unknown card %s presented at door %s" % (card_id, door['name']))
+
+    def on_start(self, door):
+        print("%s started up" % (door['name'],))
+
+    def on_alive(self, door):
+        print("%s is alive" % (door['name'],))
+
+    def on_bell(self, door):
+        print("DING DONG! Door %s" % (door['name'],))
+
+    def on_exit(self, door):
+        print("Exit button pressed on %s" % (door['name'],))
+
+dbl = TestListener()
+
+dbl.run()
