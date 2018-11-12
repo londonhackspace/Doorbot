659a880c3cd0dedf2d40e5eb2c16af26e3a11c53
Simon Hewison <simon.hewison@exactly-that.co.uk>
2018-11-12 15:18:46 +0000

    add command line parameter which is a comma-delimited list of door names to show an interest in e.g. python3 glados_listener.py 'the Hackspace First Floor Door','Nonsuch door' attempt to improve festival invocation.

--- mqttlisteners/glados_listener.py
+++ mqttlisteners/glados_listener.py
@@ -1,9 +1,17 @@
 from MQTTDoorbotListener import MQTTDoorbotListener
 import os, random, subprocess
 import logging
+import sys
 
 class GladosListener(MQTTDoorbotListener):
 
+    def __init__(self):
+        MQTTDoorbotListener.__init__(self)
+        self.doors_of_interest = sys.argv[1].split(',')
+        print ("Doors of interest: ")
+        for door in self.doors_of_interest:
+            print (".. " + door)
+            
     def getcmd(self,sound):
         # Prerequisites for this command:
         # sudo apt-get install sox libsox-fmt-mp3
@@ -16,20 +24,21 @@ class GladosListener(MQTTDoorbotListener):
             subprocess.call(self.getcmd(s))
     
     def tts(self, string):
-        subprocess.call (['festival --tts'])
+        os.system('echo "{0}" | festival --tts'.format(string))
 
     def on_card(self, card_id, name, door, gladosfile):
         if door.getboolean('announce', True):
-            print("%s presented card %s at door %s" % (name, card_id, door['name']))
-            if ( gladosfile ) :
-                g = os.getcwd()
-                os.chdir('glados-wavefiles/members')
-                self.playSounds([gladosfile])
-                os.chdir(g)
-            else:
-                # Let's try to text to speech it
-                self.tts("Greetings %s" % (name))
-
+            for a_door in self.doors_of_interest:
+                if (door['name'] == a_door):
+                    print("%s presented card %s at door %s" % (name, card_id, door['name']))
+                    if ( gladosfile ) :
+                        g = os.getcwd()
+                        os.chdir('glados-wavefiles/members')
+                        self.playSounds([gladosfile])
+                        os.chdir(g)
+                    else:
+                        # Let's try to text to speech it
+                        self.tts("Greetings %s" % (name))
         else:
             print("Will not announce stuff at %s" % (door['name'],))
 
@@ -45,15 +54,21 @@ class GladosListener(MQTTDoorbotListener):
 
     def on_bell(self, door):
         print("DING DONG! Door %s" % (door['name'],))
-        self.playSounds(["glados-wavefiles/fixed/hackspacebingbong.wav"])
+        for a_door in self.doors_of_interest:
+                if (door['name'] == a_door):
+                    self.playSounds(["glados-wavefiles/fixed/hackspacebingbong.wav"])
 
     def on_exit(self, door):
         print("Exit button pressed on %s" % (door['name'],))
-        self.playSounds(["glados-wavefiles/fixed/you're_welcome.wav"])
+        for a_door in self.doors_of_interest:
+                if (door['name'] == a_door):
+                    self.playSounds(["glados-wavefiles/fixed/you're_welcome.wav"])
 
     def on_denied(self, card_id, name, door):
         print("%s denied access with card %s at door %s" % (name, card_id, door['name']))
-        self.playSounds(["glados-wavefiles/fixed/unexpected_item_in_bagging_area.mp3"])
+        for a_door in self.doors_of_interest:
+                if (door['name'] == a_door):
+                    self.playSounds(["glados-wavefiles/fixed/unexpected_item_in_bagging_area.mp3"])
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.DEBUG)
 dbl = GladosListener()
